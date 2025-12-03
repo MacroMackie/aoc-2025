7debefd7df4768f15601116f3a97d8e57f2a40cb
Scott Mackie <scott@mackie.live>
2025-12-03 10:00:08 -0300

    wip

--- day-1/src/main_a.rs
+++ day-1/src/main_a.rs
@@ -4,42 +4,90 @@ fn main() {
     load_env();
 
     let filename = "./day-1/input/input.txt";
-    let result = calculate_calibration_values(filename).unwrap();
+    let result = calculate_dial_rotations(filename).unwrap();
 
     println!("result: {}", result);
 }
 
-pub fn calculate_calibration_values(filename: &str) -> Result<i32, Box<dyn std::error::Error>> {
+#[derive(Debug, PartialEq)]
+enum DialDirection {
+    Left,
+    Right,
+}
+
+impl DialDirection {
+    fn from_str(s: &str) -> Self {
+        if s == "L" {
+            DialDirection::Left
+        } else {
+            DialDirection::Right
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct DialRotation {
+    direction: DialDirection,
+    distance: i32,
+}
+
+pub fn calculate_dial_rotations(filename: &str) -> Result<i32, Box<dyn std::error::Error>> {
     let lines = load_file(filename)?;
 
     println!("lines: {:?}", lines);
 
-    let (mut list_a, mut list_b): (Vec<i32>, Vec<i32>) = lines
+    let array: Vec<DialRotation> = lines
         .iter()
-        .filter_map(|line| {
-            line.split_once(' ').and_then(|(first, second)| {
-                Some((
-                    first.trim().parse::<i32>().ok()?,
-                    second.trim().parse::<i32>().ok()?,
-                ))
-            })
+        .map(|line| {
+            let (direction_str, distance_str) = line.split_at(1);
+
+            let direction = DialDirection::from_str(direction_str);
+            let distance = distance_str
+                .trim()
+                .parse::<i32>()
+                .expect("Failed to parse distance");
+
+            DialRotation {
+                direction,
+                distance,
+            }
         })
-        .unzip();
+        .collect();
 
-    list_a.sort();
-    list_b.sort();
+    // We start rotating the dial from 50
+    // If we hit greater than 99 or less than 0, it wraps around to the other side
+    // (eg modulus 100)
+    let mut current_position = 50;
+    // We increment this counter whenever we turn it back to 0
+    let mut times_rotated_back_to_0 = 0;
 
-    let total_distance: i32 = list_a
-        .iter()
-        .zip(list_b.iter())
-        .map(|(a, b)| (a - b).abs())
-        .sum();
+    for rotation in array {
+        if rotation.direction == DialDirection::Left {
+            current_position -= rotation.distance;
+        } else {
+            current_position += rotation.distance;
+        }
 
-    println!("Sorted list_a: {:?}", list_a);
-    println!("Sorted list_b: {:?}", list_b);
-    println!("Total distance: {}", total_distance);
+        if current_position > 99 {
+            current_position = current_position % 100;
+        }
 
-    Ok(total_distance)
+        if current_position < 0 {
+            current_position = (current_position + 100) % 100;
+        }
+
+        if current_position == 0 {
+            times_rotated_back_to_0 += 1;
+        }
+
+        println!("rotation: {:?}", rotation);
+        println!("current_position: {}", current_position);
+    }
+
+    println!("current_position: {}", current_position);
+    println!("times_rotated_back_to_0: {}", times_rotated_back_to_0);
+
+    Ok(times_rotated_back_to_0)
 }
 
 #[cfg(test)]
@@ -50,8 +98,8 @@ mod tests {
     fn it_works() {
         load_env();
         let filename = "./day-1/input/test-a.txt";
-        let result = calculate_calibration_values(filename);
+        let result = calculate_dial_rotations(filename);
 
-        assert_eq!(result.unwrap(), 11);
+        assert_eq!(result.unwrap(), 3);
     }
 }
--- day-1/src/main_b.rs
+++ day-1/src/main_b.rs
@@ -1,63 +1,63 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
-use utils::{load_env, load_file};
+// use utils::{load_env, load_file};
 
-fn main() {
-    load_env();
+// fn main() {
+//     load_env();
 
-    let filename = "./day-1/input/input.txt";
-    let result = calculate_calibration_values(filename).unwrap();
+//     let filename = "./day-1/input/input.txt";
+//     let result = calculate_calibration_values(filename).unwrap();
 
-    println!("result: {}", result);
-}
+//     println!("result: {}", result);
+// }
 
-pub fn calculate_calibration_values(filename: &str) -> Result<i32, Box<dyn std::error::Error>> {
-    let lines = load_file(filename)?;
+// pub fn calculate_calibration_values(filename: &str) -> Result<i32, Box<dyn std::error::Error>> {
+//     let lines = load_file(filename)?;
 
-    println!("lines: {:?}", lines);
+//     println!("lines: {:?}", lines);
 
-    let (list_a, list_b): (Vec<i32>, Vec<i32>) = lines
-        .iter()
-        .filter_map(|line| {
-            line.split_once(' ').and_then(|(first, second)| {
-                Some((
-                    first.trim().parse::<i32>().ok()?,
-                    second.trim().parse::<i32>().ok()?,
-                ))
-            })
-        })
-        .unzip();
+//     let (list_a, list_b): (Vec<i32>, Vec<i32>) = lines
+//         .iter()
+//         .filter_map(|line| {
+//             line.split_once(' ').and_then(|(first, second)| {
+//                 Some((
+//                     first.trim().parse::<i32>().ok()?,
+//                     second.trim().parse::<i32>().ok()?,
+//                 ))
+//             })
+//         })
+//         .unzip();
 
-    println!("list_a: {:?}", list_a);
-    println!("list_b: {:?}", list_b);
+//     println!("list_a: {:?}", list_a);
+//     println!("list_b: {:?}", list_b);
 
-    let freq_map: HashMap<i32, usize> = list_b.iter().fold(HashMap::new(), |mut acc, &num| {
-        *acc.entry(num).or_insert(0) += 1;
-        acc
-    });
+//     let freq_map: HashMap<i32, usize> = list_b.iter().fold(HashMap::new(), |mut acc, &num| {
+//         *acc.entry(num).or_insert(0) += 1;
+//         acc
+//     });
 
-    println!("Frequency Map for List B: {:?}", freq_map);
+//     println!("Frequency Map for List B: {:?}", freq_map);
 
-    let similarity_score: i32 = list_a
-        .iter()
-        .map(|&num| num * (*freq_map.get(&num).unwrap_or(&0) as i32))
-        .sum();
+//     let similarity_score: i32 = list_a
+//         .iter()
+//         .map(|&num| num * (*freq_map.get(&num).unwrap_or(&0) as i32))
+//         .sum();
 
-    println!("Similarity Score: {}", similarity_score);
+//     println!("Similarity Score: {}", similarity_score);
 
-    Ok(similarity_score)
-}
+//     Ok(similarity_score)
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        load_env();
-        let filename = "./day-1/input/test-b.txt";
-        let result = calculate_calibration_values(filename);
+//     #[test]
+//     fn it_works() {
+//         load_env();
+//         let filename = "./day-1/input/test-b.txt";
+//         let result = calculate_calibration_values(filename);
 
-        assert_eq!(result.unwrap(), 31);
-    }
-}
+//         assert_eq!(result.unwrap(), 31);
+//     }
+// }
